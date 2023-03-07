163fe8f6606fb7e05fac5d64fc44b537a67d27f8
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2023-03-06 22:54:47 -0300

    Limpieza codigo

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 //Capturo todos los inputs de formulario
-const inputs = document.querySelectorAll('.main-container-registracion .form-control')
-const btnSubmit = document.querySelector('.btn-ingresar');
-const form = document.getElementById('form')
+const inputs = document.querySelectorAll('.main-container-componente .form-control')
+const btnConfirmar = document.querySelector('.btn-confirmar');
+const btnAgregar = document.querySelector('.btn-agregar')
+const form = document.getElementById('form-componente')
+const inputSexo = document.getElementById('input-sexo');
+const inputParentesco = document.getElementById('input-parentesco')
 
 
-//Expresiones Regulares para el formulario de Registracion
+//Expresiones Regulares para el formulario de Componentes
 let expresiones = {
-    // email: /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$/,
     nombre: /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]{1,50}$/, //Regex solo letras y espacio
-    // sexo: /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]{1,50}$/,
-    // cuit: /^[0-9]{11}$/, //Regex solo 11 numeros
     fecha: /\d{4}-\d{2}-\d{2}/,// Formate fecha dd/mm/aaaa
 }
 
-//Funcion para validar el Formulario 
+//Funcion para validar inputs sin desplegable del Formulario 
 const validarFormulario =(e) => {
    switch (e.target.name){
     case "nombre":
         validarInputs(expresiones.nombre, e.target, 'nombre')
     break;
-    // case "sexo":
-    //     validarInputs(expresiones.sexo, e.target, 'sexo')
-    // break;
-    case "cuit":
-        validarInputs(expresiones.cuit, e.target, 'cuit')
-    break;
     case "nacimiento":
         validarInputs(expresiones.fecha, e.target, 'nacimiento')
-    break;
-    case "organismo":
-        validarInputs(expresiones.organismo, e.target, 'organismo')
     break;
    }
   }
 
+  //----FUNCIONES PARA VALIDAR LOS DESPLEGABLES---
+  
+  //Funcion Validacion sexo
+const validacionSexo = (e) => {
+    validacionDesplegables(e.target, 'sexo')
+    }
 
+// Funcion Validacion Parentesco 
+const validacionParentesco = (e) => {
+    validacionDesplegables(e.target, 'parentesco')
+}
 
 
 //Funcion para comparar regex con el valor del input agregando estilos
@@ -42,61 +43,57 @@ const validarInputs = (expresion, input, campo) => {
     if(expresion.test(input.value)){
         document.getElementById(`input-${campo}`).classList.remove('is-invalid')
         document.getElementById(`input-${campo}`).classList.add('is-valid')
-        document.getElementById(`notificacion-${campo}`).classList.add('visually-hidden')
+        document.getElementById(`notificacion-${campo}`).classList.add('invisible')
     }else{
         document.getElementById(`input-${campo}`).classList.add('is-invalid')
-        document.getElementById(`notificacion-${campo}`).classList.remove('visually-hidden')
+        document.getElementById(`notificacion-${campo}`).classList.remove('invisible')
+    }
+}
+ //Funcion valida desplegables
+const validacionDesplegables = (input, campo) => {
+    if(input.value !== 'Desplegar opciones'){
+            document.getElementById(`input-${campo}`).classList.remove('is-invalid')
+            document.getElementById(`input-${campo}`).classList.add('is-valid')
+            document.getElementById(`notificacion-${campo}`).classList.add('invisible')
+        }else{
+            document.getElementById(`input-${campo}`).classList.add('is-invalid')
+            document.getElementById(`notificacion-${campo}`).classList.remove('invisible')
     }
 }
 
 
-
-//Validacion sexo
-const select = document.getElementById('input-sexo');
-const notificacion = document.getElementById('notificacion-sexo')
-
-const validacionSexo = () => {
-    if(select.value != '1' && select.value != '2'){
-        select.classList.add('is-invalid')
-        notificacion.classList.remove('visually-hidden')
-        console.log(select.value)
-    } else {
-        select.classList.remove('is-invalid')
-        select.classList.add('is-valid')
-        notificacion.classList.add('visually-hidden')
-        console.log(select.value)
-    }
-}
-
-const inputParentesco = document.getElementById('input-parentesco')
-const notificacionParentesco = document.getElementById('notificacion-parentesco')
-const validacionParentesco = () => {
-    if(inputParentesco.value != 'Desplegar opciones'){
-        select.classList.add('is-invalid')
-        notificacion.classList.remove('visually-hidden')
-        console.log(inputParentesco.value)
-    } else {
-        select.classList.remove('is-invalid')
-        select.classList.add('is-valid')
-        notificacion.classList.add('visually-hidden')
-        console.log(inputParentesco.value)
-    }
-}
-//btn para enviar la registracion
-const formRegistracion = (e) => {
+//btn para enviar los componentes agregados
+const formSubmit = (e) => {
     e.preventDefault()
     const register = document.getElementById('registracion')
     register.textContent = `Su solicitud de registracion se ha enviado correctamente`
 }
 
-// form.addEventListener('submit', formRegistracion)
+
+
+//----COMPONENTES AGREGADOS----
+
+
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario)
     input.addEventListener('blur', validarFormulario)
+
 })
 
-select.addEventListener('change', validacionSexo)
+inputSexo.addEventListener('change', validacionSexo)
 inputParentesco.addEventListener('change', validacionParentesco)
 
-// validacionSexo()
+
+let componentes = [];
+const formAgregar = (e) =>{
+    const nombre = document.getElementById('input-nombre').value
+    const nacimiento = document.getElementById('input-nacimiento').value
+    console.log('click')
+    e.preventDefault
+    componentes.push (nombre, nacimiento, inputSexo.value, inputParentesco.value)
+    console.log(componentes)
+}
+
+form.addEventListener('submit', formSubmit);
+btnAgregar.addEventListener('click', formAgregar);
